ebdbf70772b3d70df98d5b6baee6a6910b5b976c
Grayson Gullion <xinflux@gmail.com>
2022-10-26 10:51:58 -0400

    pull out styles to make jsx more ledgible

--- pages/api/og/wbiw.js
+++ pages/api/og/wbiw.js
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-/* eslint-disable import/no-anonymous-default-export */
 import { ImageResponse } from "@vercel/og"
 import cacheData from "memory-cache"
 
@@ -52,8 +51,7 @@ const fetchWithCache = async (url, options) => {
   }
 }
 
-
-export default async function(req) {
+export default async function handler(req) {
   const { searchParams } = new URL(req.url)
   const title = searchParams.get('title') || 'What is Blank Watching?'
   const response = await fetchWithCache("https://graphql.anilist.co", options)
@@ -71,58 +69,63 @@ export default async function(req) {
       return prev
     }, [])
 
+  const wrapperStyle = {
+    fontSize: 90,
+    fontWeight: 100,
+    background: 'rgb(40,40,60)',
+    color: 'white',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    overflow: 'hidden',
+  }
+
+  const containerStyle = {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    background: 'transparent',
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
+  }
+
+  const gradientStyle = {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+    background: 'linear-gradient(transparent, black)',
+  }
+
+  const textContainerStyle = {
+    position: 'absolute',
+    minWidth: '100%',
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+  }
+
+  const subtitleStyle = {
+    fontSize: 30,
+    opacity: 0.8,
+  }
+
   return new ImageResponse((
-    <div
-      style={{
-        fontSize: 90,
-        fontWeight: 100,
-        background: 'rgb(40,40,60)',
-        color: 'white',
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'relative',
-        overflow: 'hidden',
-      }}
-    >
-      <div style={{
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        background: 'transparent',
-        display: 'flex',
-        flexWrap: 'wrap',
-        width: '100%',
-        height: '100%',
-        overflow: 'hidden',
-      }}>
+    <div style={ wrapperStyle }>
+      <div style={ containerStyle }>
         { images.slice(0, 60) }
       </div>
-      
-      <div style={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        width: '100%',
-        height: '100%',
-        background: 'linear-gradient(transparent, black)',
-        
-      }}></div>
-
-      <div style={{
-        position: 'absolute',
-        minWidth: '100%',
-        textAlign: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-      }}>
-        {title}
-        <span style={{
-          fontSize: 30,
-          opacity: 0.8,
-        }}>{ images.length } shows and counting...</span>
+      <div style={ gradientStyle }></div>
+      <div style={ textContainerStyle }>
+        { title }
+        <span style={ subtitleStyle }>{ images.length } shows and counting...</span>
       </div>
     </div>
   ),{
